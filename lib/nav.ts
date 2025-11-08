00e22d710778c6dc45c52cf4d90536c491cbb869
山本陽平 <yamamotoyouhei@Mac.sugnm1.kt.home.ne.jp>
2025-11-08 10:08:25 +0900

    update footer and header categories

--- lib/nav.ts
+++ lib/nav.ts
@@ -3,7 +3,8 @@ export const CATEGORIES = [
   { en: 'UNIVERSITY',         jp: '大学生',     slug: 'university' },
   { en: 'JAPAN XV',           jp: '日本代表',   slug: 'japan' },
   { en: 'OVERSEAS',           jp: '海外',       slug: 'overseas' },
-  { en: 'HIGHSCHOOL',         jp: '高校生',     slug: 'highschool' },
+  { en: 'LEAGUEONE',           jp: 'リーグワン',       slug: 'leagueone' },
+  //{ en: 'HIGHSCHOOL',         jp: '高校生',     slug: 'highschool' },
   { en: 'COLUMN',             jp: 'コラム',     slug: 'column' },
   { en: 'TACTICS & ANALYSIS', jp: '戦術・分析', slug: 'analysis' },
 ];
