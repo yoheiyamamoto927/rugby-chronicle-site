068ead2b425e77f07f605913c002241d18cc61b2
山本陽平 <yamamotoyouhei@Mac.sugnm1.kt.home.ne.jp>
2025-12-04 23:24:32 +0900

    feat: show WP writer profile on writer category page

--- lib/nav.ts
+++ lib/nav.ts
@@ -1,8 +1,8 @@
 // lib/nav.ts
 export const CATEGORIES = [
   { en: 'UNIVERSITY',         jp: '大学生',     slug: 'university' },
-  { en: 'JAPAN XV',           jp: '日本代表',   slug: 'japan' },
-  { en: 'OVERSEAS',           jp: '海外',       slug: 'overseas' },
+  { en: 'JAPAN',           jp: '日本代表',   slug: 'japan' },
+  { en: 'INTERNATIONAL',           jp: '海外',       slug: 'international' },
   { en: 'LEAGUEONE',           jp: 'リーグワン',       slug: 'leagueone' },
   //{ en: 'HIGHSCHOOL',         jp: '高校生',     slug: 'highschool' },
   { en: 'COLUMN',             jp: 'コラム',     slug: 'column' },
